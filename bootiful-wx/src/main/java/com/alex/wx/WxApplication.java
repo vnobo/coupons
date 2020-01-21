646de10a5f5f885b97c717939f0a78b32fb442e2
AlexBob <billbob.dong@gmail.com>
2020-01-21 17:45:45 +0800

    add docker file auto build

--- bootiful-wx/src/main/java/com/alex/wx/WxApplication.java
+++ bootiful-wx/src/main/java/com/alex/wx/WxApplication.java
@@ -1,8 +1,10 @@
-package com.alex;
+package com.alex.wx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class WxApplication {
     public static void main(String[] args) {
