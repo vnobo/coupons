6a792cc263f5c4c7177a36dc925be67b17bf2b9d
AlexBob <billbob.dong@gmail.com>
2020-04-30 00:56:50 +0800

    update project files

--- ng-wx/src/app/app.component.ts
+++ ng-wx/src/app/app.component.ts
@@ -1,23 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'ng-wx';
-  user: any;
-
-  constructor(private http: HttpClient) { }
-
-  ngOnInit(): void {
-    console.log('App index init startd.')
-  }
-
-  getUser() {
-    this.http.get('user/all').subscribe(res => this.user = res);
-  }
-
+export class AppComponent {
+  isCollapsed = false;
 }
--- ng-wx/src/app/app.module.ts
+++ ng-wx/src/app/app.module.ts
@@ -4,6 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { FormsModule } from '@angular/forms';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -12,9 +23,14 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
